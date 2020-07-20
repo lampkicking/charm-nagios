cf660b465d3a7da6bc2e992d22345892e27731d8
peppepetra86 <giuseppe.petralia@canonical.com>
2020-07-20 11:52:11 +0200

    Simplify postfix_loopback_only.sh script

--- scripts/postfix_loopback_only.sh
+++ scripts/postfix_loopback_only.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 POSTFIX_CONF="/etc/postfix/main.cf"
-CODENAME="$(lsb_release -a | grep -E '^Codename:' | awk '{print $2}')"
+CODENAME="$(lsb_release -cs)"
 if [ $CODENAME = "trusty" ] ; then CMD="/etc/init.d/postfix restart" ; else CMD="systemctl restart postfix" ; fi
 
 if grep -qE '^inet_interfaces.*all' $POSTFIX_CONF; then
