797658b90aef08f87d0f1488fa3ae08a9e835b30
peppepetra86 <giuseppe.petralia@canonical.com>
2020-07-20 11:41:09 +0200

    Fix postscript script for trusty

--- scripts/postfix_loopback_only.sh
+++ scripts/postfix_loopback_only.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 POSTFIX_CONF="/etc/postfix/main.cf"
+CODENAME="$(lsb_release -a | grep -E '^Codename:' | awk '{print $2}')"
+if [ $CODENAME = "trusty" ] ; then CMD="/etc/init.d/postfix restart" ; else CMD="systemctl restart postfix" ; fi
+
 if grep -qE '^inet_interfaces.*all' $POSTFIX_CONF; then
     sed -i 's/^inet_interfaces.*/inet_interfaces = loopback-only/' $POSTFIX_CONF
-    systemctl restart postfix
+    $CMD
 fi
+
